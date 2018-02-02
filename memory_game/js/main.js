890f791816fc296e02bb7378e7bbd9ae427c050e
Benjamin Peters <benjamin.peters@gmail.com>
2018-02-02 16:54:40 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("Card Value " + cardOne);
+console.log("Card Value " + cardFour);
